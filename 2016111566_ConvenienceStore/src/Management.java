f9d9fa7bbc00861929c6c77a96d8946d915d462f
leenak0 <cdp90@naver.com>
2019-06-24 16:23:25 +0900

    파일(데이터저장)까지 완료

--- 2016111566_ConvenienceStore/src/Management.java
+++ 2016111566_ConvenienceStore/src/Management.java
@@ -1,19 +1,81 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Management {
 	private int count=0;
 	private Goods[] goodsList = new Goods[100];
 	private int sales=0;
 	
-	public void insertGoods(Goods goods) {
+	public void resetGoods() { //굿즈리스트 초기화
+		for(int i=0; i<goodsList.length; i++) {
+			if(goodsList[i] != null) {
+				goodsList[i]=null;
+			}
+		}
+		count=0;
+	}
+	
+	public void makeGoods(Goods goods) { //굿즈리스트 만들기
 		goodsList[count]=goods;
 		count++;
 	}
 	
-	public void deleteGoods(int index) {
-		goodsList[index]=null;
+	public void insertGoods(Goods goods,String file) { //물품추가
+		goodsList[count]=goods;
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(file,true));
+			bw.write(goodsList[count].getCategory()+"\t"+
+					goodsList[count].getProduct_name()+"\t"+
+					goodsList[count].getProduct_price()+"\t"+
+					goodsList[count].getProduct_stock());
+			bw.newLine();
+			bw.flush();
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		count++;
+	}
+	
+	public void deleteGoods(String product_name,String file) {
+		ArrayList<String> line=new ArrayList<String>();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			while(true) {
+				String lines=br.readLine();
+				if(lines==null) {
+					break;
+				}else if(!lines.split("\t")[1].equals(product_name)){
+					line.add(lines);
+				}
+			}
+			br.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+			for(int i=0;i<line.size();i++) {
+				bw.write(line.get(i));
+				bw.newLine();
+				bw.flush();
+			}
+			bw.close();
+		}catch(IOException ioe){
+			ioe.printStackTrace();
+		}
 	}
 	
 	public int findGoodsIndex(String product_name) {
-		int index=0;
+		int index=-1;
 		for(int i=0; i<goodsList.length; i++) {
 			if(goodsList[i] != null && goodsList[i].getProduct_name().equals(product_name)) {
 				index=i;
@@ -22,15 +84,33 @@ public class Management {
 		return index;
 	}
 	
-	public void findGoods(String category) {
+	public void findGoods(String category) throws Exception{
+		int index=-1;
 		for(int i=0; i<goodsList.length; i++) {
 			if(goodsList[i] != null && goodsList[i].getCategory().equals(category)) {
 				System.out.println(goodsList[i].getProduct_name());
+				index++;
 			}
+		}
+		if(index==-1) {
+			throw new Exception();
 		}
 	}
 	
-	public void getGoodsList() {
+	public void showGoodsList(String file) {
+		try { //데이터에 저장된 굿즈의 객체 생성
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String s;
+			while((s=br.readLine())!=null) {
+				String[]split=s.split("\t");
+				Goods goods = new Goods(split[0], split[1], Integer.valueOf(split[2]),
+						Integer.valueOf(split[3]));
+				makeGoods(goods);
+			}
+			br.close();
+		}catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
 		for(int i=0; i<goodsList.length; i++) {
 			if(goodsList[i] != null) {
 				System.out.println("상품번호["+i+"]");
@@ -47,15 +127,62 @@ public class Management {
 		return pay; //find로 찾은 index 물품의 가격*sellcount = 지불해야하는돈
 	}
 	
-	public int sell(int index, int sellCount) {
-		// 정상적으로 구매가 되면, 재고가 감소되고, 누적 총계(매출)가 증가되어야 합니다.
-		int pay=goodsList[index].getProduct_price()*sellCount;
-		if(goodsList[index].getProduct_stock()>=sellCount) { //재고 있으면
-			goodsList[index].setProduct_stock(goodsList[index].getProduct_stock()-sellCount);
-			sales += pay;
-		}else { //재고 없으면
-			System.out.println("재고가 없습니다.");
+	public void sell(int index, String product_name, int sellCount, String file, String sales) {
+		Boolean isstock=false;
+		int pay=goodsList[index].getProduct_price()*sellCount; //총가격
+		ArrayList<String> line=new ArrayList<String>();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			while(true) {
+				String lines=br.readLine();
+				if(lines==null) {
+					break;
+				}else if(lines.split("\t")[1].equals(product_name)){
+					if(Integer.parseInt(lines.split("\t")[3])>=sellCount) { //재고 있으면
+						String lines2=lines.split("\t")[0]+"\t"+lines.split("\t")[1]+"\t"+
+								lines.split("\t")[2]+"\t"+
+								(Integer.parseInt(lines.split("\t")[3])-sellCount)+"\t";
+						line.add(lines2);
+						isstock=true;
+					}else { //재고 없으면
+						line.add(lines);
+						System.out.println("재고가 없습니다.");
+					}
+				}else {
+					line.add(lines);
+				}
+			}
+			br.close();
+		}catch(Exception e){
+			e.printStackTrace();
 		}
-		return pay;//실제 판 가격
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+			for(int i=0;i<line.size();i++) {
+				bw.write(line.get(i));
+				bw.newLine();
+				bw.flush();
+			}
+			bw.close();
+		}catch(IOException ioe){
+			ioe.printStackTrace();
+		}
+		if(isstock) {
+			int sumsales = 0;
+			try {
+				BufferedReader br2 = new BufferedReader(new FileReader(sales));
+				sumsales = Integer.parseInt(br2.readLine())+pay;
+				br2.close();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			try {
+				FileWriter fw = new FileWriter(sales);
+				fw.write(String.valueOf(sumsales));
+				fw.close();
+			}catch(IOException ioe){
+				ioe.printStackTrace();
+			}
+		}
 	}
 }
--- 2016111566_ConvenienceStore/src/UserInterface.java
+++ 2016111566_ConvenienceStore/src/UserInterface.java
@@ -1,3 +1,10 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class UserInterface {
@@ -5,11 +12,35 @@ public class UserInterface {
 		Scanner scan = new Scanner(System.in);
 		Management manage=new Management();
 		boolean on = true;
-		int insert_num;
+		
+		int input_index;
+		String input_category;
+		
 		String category;
 		String product_name;
 		int product_price;
 		int product_stock;
+		Goods goods=null;
+		String file="goods.txt";
+		String sales="sales.txt";
+		
+		try { //데이터에 저장된 굿즈의 객체 생성
+		BufferedReader bi = new BufferedReader(new FileReader("goods.txt"));
+		String s;
+		while((s=bi.readLine())!=null) {
+			String[]split=s.split("\t");
+			category=split[0];
+			product_name=split[1];
+			product_price=Integer.valueOf(split[2]);
+			product_stock=Integer.valueOf(split[3]);
+			goods = new Goods(category, product_name, product_price, product_stock);
+			manage.makeGoods(goods);
+		}
+		bi.close();
+		//메소드 이동
+	}catch(IOException ioe) {
+		ioe.printStackTrace();
+	}
 		
 		while(on){
 		System.out.println("\n사용자를 선택해주세요.");
@@ -24,41 +55,49 @@ public class UserInterface {
 			System.out.println("\n구매자 메뉴입니다.");
 			System.out.println("메뉴를 선택해주세요.");
 			System.out.println("1. 상품 구매");
-			System.out.println("2. 상품 환불");
-			System.out.println("3. 종       료");
+			System.out.println("2. 종       료");
 			String con_result = scan.next();
 			switch (con_result) {
 			case "1"://상품 구매
+				manage.resetGoods();
 				System.out.println("--------상품 목록--------");
-				manage.getGoodsList();
+				manage.showGoodsList(file);
 				System.out.print("구매하실 상품의 카테고리를 입력하세요: ");
 				category = scan.next();
-				manage.findGoods(category);
+				try {
+					manage.findGoods(category);
+				} catch (Exception e) {
+					System.out.println("해당 카테고리가 존재하지 않습니다.");
+					break;
+				}
 				System.out.print("구매하실 상품의 이름을 입력하세요: ");
 				product_name = scan.next();
 				System.out.print("구매하실 상품의 수량을 입력하세요: ");
 				product_stock = scan.nextInt();
 				scan.nextLine();
-				System.out.println("지불해야 하는 총 금액은 "+
-				manage.sellEstimate(manage.findGoodsIndex(product_name),product_stock)+
-				"원 입니다.");
-				System.out.print("구매하시겠습니까?[y/n]: ");
-				switch(scan.next()) {
-				case "y":
-					manage.sell(manage.findGoodsIndex(product_name),product_stock);
-					break;
-				case "n":
-					System.out.println("구매가 취소되었습니다.");
-					break;
-				default:
-					System.out.println("입력이 잘못 되었습니다.");
+				input_index=manage.findGoodsIndex(product_name);
+				if(input_index>-1) {
+					System.out.println("지불해야 하는 총 금액은 "+
+							manage.sellEstimate(input_index,product_stock)+
+							"원 입니다.");
+							System.out.print("구매하시겠습니까?[y/n]: ");
+							switch(scan.next()) {
+							case "y":
+								manage.sell(input_index,product_name,product_stock,file,sales);
+								break;
+							case "n":
+								System.out.println("구매가 취소되었습니다.");
+								break;
+							default:
+								System.out.println("입력이 잘못 되었습니다.");
+								break;
+							}
+				} else {
+					System.out.println("해당 상품이 존재하지 않습니다.");
 					break;
 				}
 				break;
-			case "2"://상품 환불
-				System.out.println("상품을 환불합니다.");
-				break;
-			case "3"://종료
+			case "2"://종료
 				System.out.println("시스템을 종료합니다.");
 				break;
 			default:
@@ -76,7 +115,6 @@ public class UserInterface {
 			String ad_result = scan.next();
 			switch (ad_result) {
 			case "1"://상품 추가
-
 				System.out.print("상품의 카테고리를 입력하세요: ");
 				category = scan.next();
 
@@ -91,20 +129,29 @@ public class UserInterface {
 				product_stock = scan.nextInt();
 				scan.nextLine();
 				
-				Goods goods = new Goods(category, product_name, product_price, product_stock);
-				manage.insertGoods(goods);//메소드 이동
+				goods = new Goods(category, product_name, product_price, product_stock);
+				manage.insertGoods(goods,file);//메소드 이동
 				break;
 			case "2"://상품 삭제
-				System.out.print("상품의 카테고리를 입력하세요: ");
+				manage.resetGoods();
+				System.out.println("--------상품 목록--------");
+				manage.showGoodsList(file);
+				System.out.print("삭제하실 상품의 카테고리를 입력하세요: ");
 				category = scan.next();
-				manage.findGoods(category);
+				try {
+					manage.findGoods(category);
+				} catch (Exception e) {
+					System.out.println("해당 카테고리가 존재하지 않습니다.");
+					break;
+				}
 				System.out.print("삭제하실 상품의 이름을 입력하세요: ");
 				product_name = scan.next();
-				manage.deleteGoods(manage.findGoodsIndex(product_name));
+				manage.deleteGoods(product_name,file);
 				break;
 			case "3"://출력(새로 입력한 상품 리스트 전체 출력)
+				manage.resetGoods();
 				System.out.println("--------상품 목록--------");
-				manage.getGoodsList();
+				manage.showGoodsList(file);
 				break;
 			case "4"://종료
 				System.out.println("시스템을 종료합니다.");
